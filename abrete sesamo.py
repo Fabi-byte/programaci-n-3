1c0a284d9fda21e19dc6cf667c60dac3d3ec8994
Fabi-byte <74878590+Fabi-byte@users.noreply.github.com>
2020-11-22 15:52:52 -0500

    Add files via upload

--- abrete sesamo.py
+++ abrete sesamo.py
@@ -1,12 +1,14 @@
 n = 0
 a = "abrete sesamo"
+print('Cual es tu nombre?')
+nom = input ()
 while n < 10:
     print('\nCual es la palabra Magica?:')
     x = input()
     if x==a:
-        print('\nLa puerta se abre')
+        print( '\n' + nom + ' ,la puerta se abre ')
         exit ()
     else:
-        print('Incorrecto')
+        print(' \nIncorrecto ' + nom )
         n = n + 1
 exit ()
